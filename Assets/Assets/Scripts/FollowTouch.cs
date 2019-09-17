bc8761da01e9465ffc079b333ae1013486cb27e7
Julio Martins Filho <wmjulio@gmail.com>
2019-09-17 00:56:32 -0300

    Following the touch

--- Assets/Assets/Scripts/FollowTouch.cs
+++ Assets/Assets/Scripts/FollowTouch.cs
@@ -8,79 +8,47 @@ public class FollowTouch : MonoBehaviour
     public float moveSpeed = 5f;
     public float minDistance = 0.09f;
 
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
 
     Touch touch;
     Vector3 touchPosition, target;
 
-    bool isMoving = false;
-
-    float previousDistanceToTouchPos, currentDistanceToTouchPos;
+    float previousDistanceToTouchPos;
+    float currentDistanceToTouchPos;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (isMoving)
-        {
-            currentDistanceToTouchPos = (touchPosition - transform.position).magnitude;
-        }
-
-        // Prints number of fingers touching the screen
-        var fingerCount = 0;
-        foreach (Touch touch in Input.touches)
-        {
-            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
-            {
-                fingerCount++;
-            }
-        }
-        if (fingerCount > 0)
-        {
-            print("User has " + fingerCount + " finger(s) touching the screen");
-        }
-        
-
         if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
             
-            if (touch.phase == TouchPhase.Began)
-            {
-                previousDistanceToTouchPos = 0;
-                currentDistanceToTouchPos = 0;
-                isMoving = true;
+            if (
+                touch.phase == TouchPhase.Began 
+                || touch.phase == TouchPhase.Moved 
+                || touch.phase == TouchPhase.Stationary
+            ) {
                 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
                 touchPosition.z = 0;
-                target = (touchPosition - transform.position).normalized;
-                rb.velocity = new Vector2(target.x * moveSpeed, target.y * moveSpeed);
+
+                currentDistanceToTouchPos = (touchPosition - rb.transform.position).magnitude;
             }
         }
-
-        if (currentDistanceToTouchPos > previousDistanceToTouchPos)
+        
+        if (currentDistanceToTouchPos > minDistance)
         {
-            isMoving = false;
+            currentDistanceToTouchPos = (touchPosition - rb.transform.position).magnitude;
+            Vector3 dir = (touchPosition - rb.transform.position).normalized;
+            rb.MovePosition(rb.transform.position + dir * moveSpeed * Time.fixedDeltaTime);
+        } else
+        {
             rb.velocity = Vector2.zero;
         }
-
-        if (isMoving)
-        {
-            currentDistanceToTouchPos = (touchPosition - transform.position).magnitude;
-        }
     }
-    /*
-    void FixedUpdate()
-    {
-        //Find direction
-        Vector3 dir = (target.transform.position - rb.transform.position).normalized;
-        //Check if we need to follow object then do so 
-        if (Vector3.Distance(target.transform.position, rb2d.transform.position) > minDistance)
-        {
-            rb.MovePosition(rb2d.transform.position + dir * speed * Time.fixedDeltaTime);
-        }
-    }
-    */
 }
